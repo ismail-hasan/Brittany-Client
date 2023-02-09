b060d3c1078eac822eb8d238d082da1fea2b8ba7
unknown <fnismailhasan@gmail.com>
2023-02-09 22:48:02 +0600

    responsive navabr done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../Custom Hook/Button';
+import { HiOutlineBars3BottomRight, HiBars3BottomLeft } from 'react-icons/hi2';
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false)
+
     let links = [
         { name: "home", link: "#" },
         { name: "home", link: "#" },
@@ -11,9 +15,19 @@ const Header = () => {
     ]
     return (
         <div className='w-full fixed top-0 left-0 z-50'>
-            <div className='md:flex md:items-center md:justify-between bg-white  px-[100px] py-3'>
+            <div className='md:flex md:items-center md:justify-between bg-white px-[50px] md:px-[100px] py-3'>
                 <h1 className='text-[35px] font-bold'>ISMAIL</h1>
-                <ul className='md:flex md:items-center text-[18px] capitalize md:pb-0 pb-10 absolute md:static bg-white w-full md:w-auto left-0 md:pl-0 pl-[70px] transition-all duration-500 ease-in-out'>
+                <div onClick={() => setOpen(!open)} className='md:hidden text-3xl absolute right-5 top-6 transition-all duration-[5s] ease-out'>
+                    {
+                        open ?
+                            <HiOutlineBars3BottomRight ></HiOutlineBars3BottomRight>
+                            :
+                            <HiBars3BottomLeft></HiBars3BottomLeft>
+
+                    }
+                </div>
+
+                <ul className={`md:flex md:items-center text-[18px] capitalize md:pb-0 pb-10 absolute md:static bg-white w-full md:w-auto left-0 md:pl-0 pl-[25px] transition-all duration-[1s] ease-in-out ${open ? "top-16 opacity-100" : "-top-[400px] md:opacity-100 opacity-0"}`}>
                     {
                         links.map((link, idx) => {
                             return (
