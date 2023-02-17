cfd41723b6721524d341737b3955a27d6b9d48da
unknown <fnismailhasan@gmail.com>
2023-02-17 18:44:22 +0600

    heading updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div className='h-screen text-[#CCD6F6] flex justify-center md:mt-[35px] flex-col  w-[85%] md:w-[75%]'>
-            <h1 className=''>Hi, my name is</h1>
+            <h1 className='text-[#40ff00]'>Hi, my name is</h1>
             <h1 className='text-[28px] md:text-[53px] font-bold tracking-[2px] capitalize'>Ismail Hossain</h1>
             <h1 className='text-[28px] md:text-[53px] capitalize'>I will Design Your imagination</h1>
             <p className='text-lg mt-5'>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
